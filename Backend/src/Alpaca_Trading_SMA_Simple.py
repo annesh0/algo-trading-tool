20f496769c0d1c320010bd91e06c6a5c4407223c
Annesh Ghosh Dastidar <ag835@cornell.edu>
2023-01-01 08:25:25 +0530

    Added template

--- Backend/src/Alpaca_Trading_SMA_Simple.py
+++ Backend/src/Alpaca_Trading_SMA_Simple.py
@@ -1,57 +1,25 @@
 import config as cf
-import alpaca_trade_api as tradeapi
+from alpaca.trading.client import TradingClient
+from alpaca.trading.requests import MarketOrderRequest
+from alpaca.trading.enums import OrderSide, TimeInForce
 import matplotlib.pyplot as plt
 import numpy as np
 
 # Base URL for fetching data, portfolio, etc. from Alpaca
 BASE_URL = "https://paper-api.alpaca.markets"
 
-# Create REST API connection
-api = tradeapi.REST(key_id=cf.ALPACA_API_KEY,
-                    secret_key=cf.ALPACA_SECRET_KEY, base_url=BASE_URL, api_version='v2')
+API_KEY = "<Your API Key>"
+SECRET_KEY = "<Your Secret Key>"
 
-# Fetch Apple data from last 20 days
-APPLE_DATA = api.get_barset('AAPL', 'day', limit=100).df
+trading_client = TradingClient(API_KEY, SECRET_KEY, paper=True)
 
-# Reformat data (drop multiindex, rename columns, reset index)
-APPLE_DATA.columns = APPLE_DATA.columns.to_flat_index()
-APPLE_DATA.columns = [x[1] for x in APPLE_DATA.columns]
-APPLE_DATA.reset_index(inplace=True)
+market_order_data = MarketOrderRequest(
+                      symbol="BTC/USD",
+                      qty=1,
+                      side=OrderSide.BUY,
+                      time_in_force=TimeInForce.GTC
+                  )
 
-# Calculate moving averages
-APPLE_DATA['20_SMA'] = APPLE_DATA['close'].rolling(window=20, min_periods=1).mean()
-APPLE_DATA['10_SMA'] = APPLE_DATA['close'].rolling(window=10, min_periods=1).mean()
-
-# Find crossover points
-APPLE_DATA['Cross'] = 0.0
-APPLE_DATA['Cross'] = np.where(APPLE_DATA['10_SMA'] > APPLE_DATA['20_SMA'], 1.0, 0.0)
-APPLE_DATA['Signal'] = APPLE_DATA['Cross'].diff()
-
-# Map numbers to words
-map_dict = {-1.0: 'sell', 1.0: 'buy', 0.0: 'none'}
-APPLE_DATA["Signal"] = APPLE_DATA["Signal"].map(map_dict)
-
-# Preview Data
-# print(APPLE_DATA.head())
-
-# Show Relevant Buy/Sell data
-print(APPLE_DATA[APPLE_DATA['Signal'] != 'none'].dropna())
-
-
-# Plot stock price data
-APPLE_DATA.plot(x="time", y=["close", "20_SMA", "10_SMA"], color=['k', 'b', 'm'])
-
-# Plot ‘buy’ signals
-plt.plot(APPLE_DATA[APPLE_DATA['Signal'] == 'buy']['time'],
-         APPLE_DATA['20_SMA'][APPLE_DATA['Signal'] == 'buy'],
-         '^', markersize=8, color='g', label='buy')
-
-# Plot ‘sell’ signals
-plt.plot(APPLE_DATA[APPLE_DATA['Signal'] == 'sell']['time'],
-         APPLE_DATA['20_SMA'][APPLE_DATA['Signal'] == 'sell'],
-         'v', markersize=8, color='r', label='sell')
-
-plt.xlabel("Date")
-plt.ylabel("Apple Close Price ($)")
-plt.legend()
-plt.show()
+market_order = trading_client.submit_order(market_order_data)
+for property_name, value in market_order:
+  print(f"\"{property_name}\": {value}")
