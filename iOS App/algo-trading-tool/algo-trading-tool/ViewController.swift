e10b8afbc0e2929442c9d239e4c4f2a73a081932
Annesh Ghosh Dastidar <ag835@cornell.edu>
2023-01-05 12:13:41 +0530

    Added ios files

---
+++ iOS App/algo-trading-tool/algo-trading-tool/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  algo-trading-tool
+//
+//  Created by Annesh Ghosh Dastidar on 1/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
