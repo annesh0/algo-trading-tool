5e7f728f904f935c57947cce53ac9e342decdcfb
Annesh Ghosh Dastidar <ag835@cornell.edu>
2023-01-05 12:35:21 +0530

    Updating dashboard

--- Dashboard/my-app/src/layouts/dashboard/DashboardLayout.js
+++ Dashboard/my-app/src/layouts/dashboard/DashboardLayout.js
@@ -8,8 +8,8 @@ import Nav from './nav';
 
 // ----------------------------------------------------------------------
 
-const APP_BAR_MOBILE = 64;
-const APP_BAR_DESKTOP = 92;
+const APP_BAR_MOBILE = 14;
+const APP_BAR_DESKTOP = 12;
 
 const StyledRoot = styled('div')({
   display: 'flex',
@@ -37,7 +37,7 @@ export default function DashboardLayout() {
 
   return (
     <StyledRoot>
-      <Header onOpenNav={() => setOpen(true)} />
+      {/* <Header onOpenNav={() => setOpen(true)} /> */}
 
       <Nav openNav={open} onCloseNav={() => setOpen(false)} />
 
--- Dashboard/my-app/src/layouts/dashboard/nav/config.js
+++ Dashboard/my-app/src/layouts/dashboard/nav/config.js
@@ -12,30 +12,35 @@ const navConfig = [
     icon: icon('ic_analytics'),
   },
   {
-    title: 'user',
+    title: 'strategies',
     path: '/dashboard/user',
     icon: icon('ic_user'),
   },
   {
-    title: 'product',
-    path: '/dashboard/products',
-    icon: icon('ic_cart'),
-  },
-  {
-    title: 'blog',
-    path: '/dashboard/blog',
-    icon: icon('ic_blog'),
-  },
-  {
-    title: 'login',
-    path: '/login',
+    title: 'Settings',
+    path: '/dashboard/settings',
     icon: icon('ic_lock'),
   },
-  {
-    title: 'Not found',
-    path: '/404',
-    icon: icon('ic_disabled'),
-  },
+  // {
+  //   title: 'product',
+  //   path: '/dashboard/products',
+  //   icon: icon('ic_cart'),
+  // },
+  // {
+  //   title: 'blog',
+  //   path: '/dashboard/blog',
+  //   icon: icon('ic_blog'),
+  // },
+  // {
+  //   title: 'login',
+  //   path: '/login',
+  //   icon: icon('ic_lock'),
+  // },
+  // {
+  //   title: 'Not found',
+  //   path: '/404',
+  //   icon: icon('ic_disabled'),
+  // },
 ];
 
 export default navConfig;
