5575d76203724d14a27ff00184fde9e9a6cb85f2
Annesh Ghosh Dastidar <ag835@cornell.edu>
2023-02-02 11:44:47 -0500

    Refactor

--- Backend/src/alpaca-proxy/shared/data.py
+++
@@ -1,7 +0,0 @@
-import queue
-from collections import defaultdict
-
-subscribers = defaultdict(list)
-q_mapping = {}
-register_queue = queue.Queue()
-response_queue = queue.Queue()
---
+++ Backend/src/core/strategy.py
@@ -0,0 +1,7 @@
+class strategy:
+    def __init__(self) -> None:
+        portfolio = []
+        balance = 0
+        history = []
+        name = ""
+    #for each asset, we need entry price?
